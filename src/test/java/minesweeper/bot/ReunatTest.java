f6653b693628b3fc1197a721cdcbe7579e29375a
Insanefun <jartsa97@gmail.com>
2020-02-01 17:58:21 +0200

    Testailua laajennettu

---
+++ src/test/java/minesweeper/bot/ReunatTest.java
@@ -0,0 +1,44 @@
+
+package java.minesweeper.bot;
+
+import java.util.HashSet;
+import junit.framework.TestCase;
+import minesweeper.generator.MinefieldGenerator;
+import minesweeper.model.Board;
+import minesweeper.model.Square;
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ * @author User
+ */
+public class ReunatTest extends TestCase {
+    private MinefieldGenerator generator;
+    private Board board;
+    public ReunatTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        this.generator = new MinefieldGenerator();
+        this.board = new Board(generator, 5, 5, 0);
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    // TODO add test methods here. The name must begin with 'test'. For example:
+    // public void testHello() {}
+    public void testMiina(){
+        Reunat k = new Reunat();
+        board.getSquareAt(2, 2).setMine();
+        board.getSquareAt(2, 3).open();
+        Square q = board.getSquareAt(2, 3);
+        HashSet<Square> reunalla = k.laske(board);
+        assertTrue(reunalla.contains(q));
+    }
+}
